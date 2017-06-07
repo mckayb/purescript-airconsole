8468a23ef798c3a13f60935c6a89f2ff1c8c596a
McKay Broderick <mckay.db@gmail.com>
2017-06-06 21:50:46 -0600

    Changed various methods to not return eff when they dont need to. Added nullable signatures for when airconsole returns undefined.

--- src/AirConsole/ActivePlayers.purs
+++ src/AirConsole/ActivePlayers.purs
@@ -2,13 +2,14 @@ module AirConsole.ActivePlayers where
 
 import Control.Monad.Eff (Eff)
 import AirConsole.Types (AirConsoleGlobal, PlayerNumber, DeviceId)
-import AirConsole.FFI (runFn0, runEffFn1)
+import AirConsole.FFI (runFn0, runFn1, runEffFn1)
+import Data.Nullable (Nullable)
 
-convertDeviceIdToPlayerNumber :: forall e a. AirConsoleGlobal -> DeviceId -> Eff e a
-convertDeviceIdToPlayerNumber x n = runEffFn1 "convertDeviceIdToPlayerNumber" x n
+convertDeviceIdToPlayerNumber :: AirConsoleGlobal -> DeviceId -> Nullable PlayerNumber
+convertDeviceIdToPlayerNumber x n = runFn1 "convertDeviceIdToPlayerNumber" x n
 
-convertPlayerNumberToDeviceId :: forall e a. AirConsoleGlobal -> PlayerNumber -> Eff e a
-convertPlayerNumberToDeviceId x n = runEffFn1 "convertPlayerNumberToDeviceId" x n
+convertPlayerNumberToDeviceId :: AirConsoleGlobal -> PlayerNumber -> Nullable DeviceId
+convertPlayerNumberToDeviceId x n = runFn1 "convertPlayerNumberToDeviceId" x n
 
 getActivePlayerDeviceIds :: AirConsoleGlobal -> Array DeviceId
 getActivePlayerDeviceIds x = runFn0 "getActivePlayerDeviceIds" x
